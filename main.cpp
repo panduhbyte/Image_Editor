2f73f2589f4db048f43f533355b52290c74d6e2c
Char-kun <panduhbyte@gmail.com>
2017-10-15 23:32:47 -0400

    Set Public Class
    DESKTOP: Setup Image class. Requires to define functions within it.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,36 @@
 #include <string>
 using namespace std;
 
+class Image{
+private:
+    int width, height;  //image dimensions
+    int *image_array;   //pointer to the dynamically allocated image array
+
+public:
+    Image();    //constructor - makes empty image
+                //makes an image object by reading input file
+    Image(string input_file);
+                //makes an image object using dimensions
+    Image(int width, int height);
+    ˜Image ();  //destructor - provides as many destructors needed
+
+                //accessors / mutators
+    int getWidth();
+    void setWidth(int w);
+    int getHeight();
+    void setHeight(int h);
+
+                //set/get an image pixel by row and col address
+                //pixel is a 3 element r,g,b triple
+    void getImagePixel(int col, int row, int *pixel);
+    void setImagePixel(int col, int row, int *pixel);
+
+
+    read(string infile);
+    write(string outfile);
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+
     return 0;
 }
