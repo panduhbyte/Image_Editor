3bd6a74bce540417901e1c65a5d968800d26b9a3
Char-kun <panduhbyte@gmail.com>
2017-10-15 20:51:43 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Image_Editor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image_Editor.dir/main.cpp.obj"
+  "Image_Editor.pdb"
+  "Image_Editor.exe"
+  "Image_Editor.exe.manifest"
+  "libImage_Editor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image_Editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
