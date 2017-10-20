2d6f3c6fe20a303bf5169ceb03696d794a0595da
Char-kun <panduhbyte@gmail.com>
2017-10-19 23:44:01 -0400

    Read Function
    Read function set, reads in correct formatting of rgb. - Desktop

--- main.cpp
+++ main.cpp
@@ -1,41 +1,54 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstdlib>
+#include <stdlib.h>
 using namespace std;
 
 class Image{
 private:
-    int width, height;  //image dimensions
+    int width, height, col, row, max, ppm_w;  //image dimensions
     int **image_array;   //pointer to the dynamically allocated image array
+    string  temp_file, magic_number;
+    ifstream infile;
+    ofstream outfile;
 
 public:
-    Image();    //constructor - makes empty image
-                //makes an image object by reading input file
-    Image(string input_file);
-                //makes an image object using dimensions
-    Image(int width, int height);
+    Image(){
+
+    };
 
     ~Image();  //destructor - provides as many destructors needed
 
-                //accessors / mutators
-    int getWidth();
-    void setWidth(int w);
-    int getHeight();
-    void setHeight(int h);
-
                 //set/get an image pixel by row and col address
                 //pixel is a 3 element r,g,b triple
-    getImagePixel(int col, int row, int *pixel);
-    void setImagePixel(int col, int row, int *pixel);
+    void getImagePixel();
+    void setImagePixel();
 
 
-    read(string infile){
+    void read(){
+        infile.open("yosemite.ppm");
+        if(infile.is_open()){
+            infile >> magic_number;
+            infile >> width;
+            infile >> height;
+            infile >> max;
+            ppm_w = width * 3;
+            for(int rowNum=0;rowNum<height;rowNum++){
+                for(int colNum=0;colNum<ppm_w;colNum++){
+                        infile >> image_array[rowNum][colNum];
+                }
+            }
+        }//End of if infile.is_open
+        infile.close();
+        infile.clear();
+    };
+
+    void write(){
 
     };
-    write(string outfile){
 
     };
-};
 
 int main() {
 
